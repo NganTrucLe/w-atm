e636af10adbb0ceff18e7fdb130a87144c418132
vmathu <anhthuvominh1@gmail.com>
2022-09-11 08:46:24 +0700

    feat: Hiển thị map ở vị trí hiện tại

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,14 +1,60 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class MapScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+class MapScreen extends StatefulWidget {
   static const routeName = '/map';
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  late LatLng currentLocation;
+
+  final List<Marker> _markers = <Marker>[];
+
+  void _addNewMarker(String title, Position location) {
+    final newMarker = Marker(
+        markerId: MarkerId((_markers.length + 1).toString()),
+        position: LatLng(location.latitude, location.longitude),
+        infoWindow: InfoWindow(title: title));
+
+    setState(() {
+      _markers.add(newMarker);
+    });
+  }
+
+  @override
+  void initState() {
+    Geolocator.getCurrentPosition().then((currLocation) {
+      setState(() {
+        currentLocation = LatLng(currLocation.latitude, currLocation.longitude);
+        _addNewMarker('My current location', currLocation);
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text('Map'),
-      ),
+      body: currentLocation == null
+          ? const Center(child: CircularProgressIndicator())
+          : GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: currentLocation, zoom: 14),
+              markers: Set<Marker>.of(_markers),
+              myLocationEnabled: true,
+              compassEnabled: true,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
     );
   }
 }
