440b0b98b49058d42a98ea9ff8aec0ac4e9a4fb1
lengocthao.dev <lengocthao.dev@gmail.com>
2022-09-12 17:29:59 +0700

    theme: thêm fonts và bảng màu

---
+++ lib/theme/colors.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Primary
+  final primary500 = const Color(0xFF5200FF);
+  final primary400 = const Color(0xFF7433FF);
+  final primary300 = const Color(0xFF9766FF);
+  final primary200 = const Color(0xFFBA99FF);
+  final primary100 = const Color(0xFFDCCCFF);
+  final primary600 = const Color(0xFF310098);
+  final primary700 = const Color(0xFF210066);
+  final primary800 = const Color(0xFF100033);
+  //Error
+  final error800 = const Color(0xFF7F1409);
+  final error700 = const Color(0xFFB21B0D);
+  final error600 = const Color(0xFFCB1F0E);
+  final error500 = const Color(0xFFFE2712);
+  final error400 = const Color(0xFFFE5241);
+  final error300 = const Color(0xFFFE7D71);
+  final error200 = const Color(0xFFFFBEB8);
+  //Warning
+  final warning800 = const Color(0xFF7F4D00);
+  final warning700 = const Color(0xFFB16B00);
+  final warning600 = const Color(0xFFCA7A00);
+  final warning500 = const Color(0xFFFD9900);
+  final warning400 = const Color(0xFFFDAD33);
+  final warning300 = const Color(0xFFFEC266);
+  final warning200 = const Color(0xFFFEE0B3);
+  //Success
+  final success200 = const Color(0xFFD1EBBE);
+  final success300 = const Color(0xFFA2D77D);
+  final success400 = const Color(0xFF83C951);
+  final success500 = const Color(0xFF64BC26);
+  final success600 = const Color(0xFF46841B);
+  final success700 = const Color(0xFF46841B);
+  final success800 = const Color(0xFF325E13);
+  //Neutral
+  final white = const Color(0xFFFFFFFF);
+  final neutral800 = const Color(0xFFCCCCCC);
+  final neutral600 = const Color(0xFF999999);
+  final neutral500 = const Color(0xFF808080);
+  final neutral300 = const Color(0xFF4D4D4D);
+  final neutral200 = const Color(0xFF333333);
+  final neutral120 = const Color(0xFF1F1F1F);
+  const AppColors();
+}
