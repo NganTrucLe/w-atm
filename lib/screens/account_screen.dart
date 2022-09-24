cd262441b81763f12b68c2413ed7574eb6fcaa68
ngantruc2003 <ngantruc2003@gmail.com>
2022-09-24 14:06:43 +0700

    dummy account screen

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:watm/theme/theme_constants.dart';
 
 class AccountScreen extends StatelessWidget {
   static const routeName = '/account';
-  
+  static const listOptions = [
+    'Connect bank',
+    'History',
+    'Language',
+    'Clear cache',
+    'Terms & Privacy Policy',
+    'Contact us',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +24,27 @@ class AccountScreen extends StatelessWidget {
           color: Colors.black,
         ),
       ),
-      body: Container(
-        child: Text('Account'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: 
+            listOptions.map((option) => Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(width: 1, color: AppTheme.colors.neutral800),
+                )
+              ),
+              child: ListTile(
+                title: Text(option),
+                trailing: Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  size: 24,
+                  color: AppTheme.colors.neutral800,
+                ),
+              ),
+            )
+            ).toList(),
+        ),
       ),
     );
   }
