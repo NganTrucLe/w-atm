b6542dac2955fd50fa52a981cd034742028d5838
ngantruc2003 <ngantruc2003@gmail.com>
2022-09-25 08:36:03 +0700

    fix overflow

--- lib/widgets/atm_result.dart
+++ lib/widgets/atm_result.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:watm/theme/theme_constants.dart';
 
 import '../models/atm.dart';
 import '../screens/atm_details_screen.dart';
@@ -6,7 +7,7 @@ import '../screens/atm_details_screen.dart';
 class ATMResult extends StatelessWidget {
   final ATM ATMInfo;
   final String name;
-  
+
   ATMResult(this.ATMInfo, this.name);
 
   void selectATM(BuildContext context, ATM ATMInfo) {
@@ -22,38 +23,60 @@ class ATMResult extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () => selectATM(context, ATMInfo),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Column(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 6),
+            child: Row(
               children: [
-                CircleAvatar(
-                  child: Icon(Icons.location_pin),
+                Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 16,
+                      child: Icon(
+                        Icons.location_pin,
+                        color: Colors.white,
+                      ),
+                      backgroundColor: AppTheme.colors.primary500,
+                    ),
+                    Text('x km'),
+                  ],
                 ),
-                Text('x km'),
+                SizedBox(
+                  width: 12,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        name,
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                      Text(
+                        '${ATMInfo.address}',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: AppTheme.colors.neutral500,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
-            SizedBox(
-              width: 10,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(name,
-                    style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    )),
-                Text('${ATMInfo.address}', style: TextStyle(fontSize: 15)),
-              ],
-            ),
-          ],
-        ),
+          ),
+          Divider(height: 1, color: AppTheme.colors.neutral800)
+        ],
       ),
     );
   }
 }
-
-
